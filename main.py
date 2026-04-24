bf2c532c744af30257ef6847f3ed9cef99e45b54
Sammy-Shieunda <sammyshieunda@gmail.com>
2026-04-23 21:49:52 -0700

    correction 1

--- main.py
+++ main.py
@@ -64,8 +64,7 @@ pd.read_sql("SELECT * FROM dogs;", conn2)
 # Replace None with your code
 df_hungry = pd.read_sql("""
 SELECT name, age, breed FROM dogs
-WHERE hungry = 1
-ORDER BY age ASC;
+WHERE hungry = 1;
 """, conn2)
 
 # CodeGrade step7
